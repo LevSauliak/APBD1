32e8bf4329f493c9ff654adc57e258d2a539eac6
s30243 <s30243@pjwstk.edu.pl>
2025-03-03 16:26:06 +0100

    new method Average

--- Program.cs
+++ Program.cs
@@ -2,3 +2,13 @@
 String name = Console.ReadLine();
 Console.WriteLine("Your name is " + name);
 Console.WriteLine("Byeee");
+
+static int Average(int[] nums)
+{
+    int sum = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        sum += nums[i];
+    }
+    return sum/nums.Length;
+}
